26be2e56fc584b1fa9d6e1f9bbcc5b608024c391
novailoveyou <novailoveyoux@gmail.com>
2021-05-24 00:48:30 +0300

    got rid of listDatabases func

--- app.js
+++ app.js
@@ -5,10 +5,3 @@ const { Client } = require('@notionhq/client')
 const notion = new Client({
   auth: process.env.NOTION_TOKEN
 })
-
-const listDatabases = async () => {
-  const res = await notion.databases.list()
-  console.log(res)
-}
-
-listDatabases()
