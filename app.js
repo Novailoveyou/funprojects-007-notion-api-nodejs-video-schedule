ddfbe60536cce0fb3386ca695dabb89b297a1df8
novailoveyou <novailoveyoux@gmail.com>
2021-05-29 10:30:06 +0300

    using the SDK/Client

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ const { Client } = require('@notionhq/client')
 const notion = new Client({
   auth: process.env.NOTION_TOKEN
 })
+
+const listDatabases = async () => {
+  const res = await notion.databases.list()
+  console.log(res)
+}
+
+listDatabases()
